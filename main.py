e68a86cf14aeb9d55d37b7ed3381c987caddf9ec
habib-gm <habib.gemmech@gmail.com>
2022-01-02 00:45:39 +0300

    creating Turtle object and setting it's property for second

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ time_now = datetime.datetime.now()
 Hours = int(time_now.strftime("%I"))
 Minutes = int(time_now.strftime("%M"))
 Seconds = int(time_now.strftime("%S"))
+#creating Turtle object and setting it's property for second ---> I used arrow
+tsecond = turtle.Turtle("arrow")
+tsecond.shapesize(0.2, 16)
+tsecond.color("orange")
+second_angle = 90
